3a776b1dec079b0e22449cf104df037049c72199
raisa <jannatur.cse.202201040144@aust.edu>
2026-08-09 22:44:05 +0600

    sidebar fix

--- Admin/src/routes/AppRoutes.jsx
+++ Admin/src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import AdminLayout from "../layouts/AdminLayout";
 
 import Dashboard from "../pages/Dashboard";
@@ -7,57 +8,54 @@ import Orders from "../pages/Orders";
 import Messages from "../pages/Messages";
 import Login from "../pages/Login";
 
-import AdminProtectedRoute from "../components/AdminProtectedRoute";
-
-
 export default function AppRoutes() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
 
-        {/* =========================
-            ADMIN 
-        ========================= */}
-
+        {/* Default */}
         <Route
-            path="/"
-            element={<Navigate to="/admin" replace />}
-          />
+          path="/"
+          element={<Navigate to="/admin" replace />}
+        />
 
-          {/* =========================
-              ADMIN LOGIN
-          ========================= */}
-          <Route
-            path="/admin/login"
-            element={<Login />}
-          />
+        {/* Admin Login */}
+        <Route
+          path="/admin/login"
+          element={<Login />}
+        />
 
+        {/* Admin Layout */}
+        <Route
+          path="/admin"
+          element={<AdminLayout />}
+        >
+          {/* /admin */}
           <Route
             index
             element={<Dashboard />}
           />
 
+          {/* /admin/products */}
           <Route
             path="products"
             element={<Products />}
           />
 
+          {/* /admin/orders */}
           <Route
             path="orders"
             element={<Orders />}
           />
 
+          {/* /admin/messages */}
           <Route
             path="messages"
             element={<Messages />}
           />
+        </Route>
+
       </Routes>
-
     </BrowserRouter>
-
   );
-
 }
